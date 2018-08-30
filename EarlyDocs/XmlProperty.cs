f82bd07fd91932bdebea5c233d81352322617bd4
J M <wohaste@gmail.com>
2018-08-30 09:00:33 -0600

    support nullable types on properties

--- EarlyDocs/XmlProperty.cs
+++ EarlyDocs/XmlProperty.cs
@@ -18,6 +18,14 @@ namespace EarlyDocs
 		{
 			IsConstant = false;
 			DataTypeName = propertyInfo.PropertyType.Name;
+			if(DataTypeName.Contains("Nullable"))
+			{
+				//FullName example: System.Nullable`1[[System.Drawing.Color, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a]]
+				string innerType = propertyInfo.PropertyType.FullName;
+				innerType = innerType.Substring(innerType.IndexOf("[[") + 2);
+				innerType = innerType.Substring(0, innerType.IndexOf(","));
+				DataTypeName = innerType + "?";
+			}
 		}
 	}
 }
